f31c21a3d7bcba96346dc90756c948d74626c5fe
Fadhil <fadhil.akins@gmail.com>
2020-09-16 15:58:42 +0100

    uhm okayy

--- JS/script.js
+++ JS/script.js
@@ -1,46 +1,52 @@
 const API="https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
-const canvasHeight=700,
-canvasWidth =750,
-padding=50;
+const canvasHeight=820,
+canvasWidth =650,
+padding=50,
+specifier = "%M:%S",
+timeFormat = d3.timeFormat("%M:%S");
+var y = d3.scaleTime()
+  .range([0, canvasHeight]);
 
 
 d3.json(API, (err,data) => {
  const TIME = data.map(item=>item.Time);
  const YEAR= data.map(item=>item.Year);
-  
- const Specifier = "%M:%S";
- console.log(TIME);
 
- const parseTime =TIME.map(time=>{
-  return d3.timeParse(Specifier)(time);
-})
+data.forEach(function(d) {
+    d.Place = +d.Place;
+    var parsedTime = d.Time.split(':');
+    d.Time = new Date(1970, 0, 1, 0, parsedTime[0], parsedTime[1]);
+  });
+
 
 const xScale = d3.scaleLinear();
-xScale.domain([d3.min(YEAR), d3.max(YEAR)]);
+xScale.domain([d3.min(YEAR)-1, d3.max(YEAR)+1]);
 xScale.range([padding, canvasWidth-padding])
- 
-const yScale = d3.scaleTime();
-yScale.domain(d3.extent(parseTime))
+
+const xAxis = d3.axisBottom(xScale); 
+
+y.domain(d3.extent(data, function(d) {
+    return d.Time;
+  }));
+const parseTime =TIME.map(time=>{
+  return d3.timeParse(specifier)(time);
+})
+
+
+ const yScale = d3.scaleTime();
+yScale.domain(d3.extent(parseTime, (time)=>{console.log(time); return time}))
 .range([padding, canvasHeight-padding]);
 
+const yAxis = d3.axisLeft(y).tickFormat(timeFormat)
+
 const dataset = parseTime.map((item, index)=>{
-  return([yScale(item), xScale(YEAR[index])]);
+  return([xScale(YEAR[index]), yScale(item)]);
 });
 
-const xAxis = d3.axisBottom(xScale);
-      
 
-/*const gX = svg.append("g")
-  .attr("transform", "translate(0,50)")
-  .call(axis);*/
 
-const yAxis = d3.axisLeft(yScale)
-.tickValues(parseTime)
-      .tickFormat((d,i)=>{
-        return TIME[i]
-      });
 
-console.log(dataset);
+
   
   const CANVAS =  d3.select(".canvas")
    .append("svg")
@@ -52,8 +58,8 @@ console.log(dataset);
    .data(dataset)
    .enter()
    .append("circle")
-   .attr("cx", d => d[1])
-   .attr("cy", d=>  d[0])
+   .attr("cx", d => d[0])
+   .attr("cy", d=>  d[1])
    .attr("r", "6");
    
    CANVAS.append("g")
